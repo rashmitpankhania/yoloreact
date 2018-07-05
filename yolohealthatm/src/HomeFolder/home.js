e2dd23374378a6f563614bef7deae2594097017f
YoloHealth <auto@yolohealth.in>
2018-07-05 13:02:30 +0530

    updated

--- yolohealthatm/src/HomeFolder/home.js
+++ yolohealthatm/src/HomeFolder/home.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Placeholder Home
-                <ul>
-                    <li><HomeMenu/></li>
-                    <li><HomeMenu/></li>
-                    <li><HomeMenu/></li>
-                </ul>
+                    <div >
+                     <HomeMenu name="consult"/>
+                    </div>
+                    <div >
+                    <HomeMenu name="checkup"/>
+                    </div>
+                    <div >
+                    <HomeMenu name="history"/>
+                    </div>
+
+                   
+              
             </div>
         )
     }
 }
 
 class HomeMenu extends Component {
-    render() {
+    render(prpos) {
         return (
-            <div>placeholder for Home</div>
+            
+            <div>{this.props.name}
+              
+                <Router>
+                     <Route  path={this.props.name}  />
+                </Router>
+            </div>
         )
     }
 }
--- yolohealthatm/src/index.js
+++ yolohealthatm/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Switch, BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
