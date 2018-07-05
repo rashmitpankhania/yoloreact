f77474a5985fedba731d07ac0aa649169b917284
Rashmit Pankhania <rashmitpankhania@gmail.com>
2018-07-05 13:05:49 +0530

    Auto stash before merge of "master" and "origin/master"

--- yolohealthatm/src/index.js
+++ yolohealthatm/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RoutedApp from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Switch, BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RoutedApp />, document.getElementById('root'));
 registerServiceWorker();
